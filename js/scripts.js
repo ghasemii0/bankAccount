1211f7b900084e37210f0e56e62ab390b000be7a
Mustafa Ghasemi and Stuart McKay <stuart@>
2019-04-09 15:29:00 -0700

    add ability to create account

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 // Business Logic
+function Bank() {
+  this.bankAccounts = [],
+  this.currentId = 0
+}
+
+Bank.prototype.addAccount = function(bankAccount) {
+  bankAccount.id = this.assignId();
+  this.bankAccounts.push(bankAccount);
+}
+
+Bank.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
 function BankAccount(name, amount) {
-  this.name,
-  this.amount
-};
+  this.name = name,
+  this.amount = amount
+}
 
 BankAccount.prototype.deposit = function(userDeposit) {
   if (userDeposit > 0) {
@@ -23,3 +38,22 @@ BankAccount.prototype.withdraw = function(userWithdraw) {
 BankAccount.prototype.showMeTheMoney = function() {
   Alert(this.amount);
 }
+
+var bank = new Bank();
+// User Interface
+$(document).ready(function(){
+  $("form#accountRegistration").submit(function(event){
+    event.preventDefault();
+    var name = $("input#name").val();
+    var amount = $("input#initialDeposit").val();
+    var account = new BankAccount(name, amount);
+    bank.addAccount(account);
+    console.log(bank);
+  });
+  $("form#balanceChange").submit(function(event){
+    event.preventDefault();
+    var depositAmount = $("input#deposit").val();
+    var withdrawal = $("input#withdrawal").val();
+    console.log(depositAmount, withdrawal);
+  });
+});
