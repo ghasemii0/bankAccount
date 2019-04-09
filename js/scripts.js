22d5d66dd7c60db6da646e12a0a56df24f464c28
Mustafa Ghasemi and Stuart McKay <mustafa@>
2019-04-09 16:46:12 -0700

    allow to add more money to an existing account by name search. Still no withdrawal feature

--- js/scripts.js
+++ js/scripts.js
@@ -14,22 +14,31 @@ Bank.prototype.assignId = function() {
   return this.currentId;
 }
 
+Bank.prototype.findAccount = function(name) {
+  for (var i=0; i< this.bankAccounts.length; i++) {
+      if (this.bankAccounts[i].name == name) {
+        return this.bankAccounts[i];
+      }
+    };
+  return false;
+}
+
 function BankAccount(name, amount) {
   this.name = name,
   this.amount = amount
 }
 
-BankAccount.prototype.deposit = function(userDeposit) {
-  if (userDeposit > 0) {
-    this.amount += userDeposit;
+BankAccount.prototype.deposit = function(depositAmount) {
+  if (depositAmount > 0) {
+    this.amount += depositAmount;
   } else {
     console.log("Please select at least $1 to deposit.");
   }
 };
 
-BankAccount.prototype.withdraw = function(userWithdraw) {
-  if (this.amount - userWithdraw >= 0) {
-    this.amount -= userWithdraw;
+BankAccount.prototype.withdraw = function(withdrawalAmount) {
+  if (this.amount - withdrawalAmount >= 0) {
+    this.amount -= withdrawalAmount;
   } else {
     console.log("Help! They're trying to rob us!");
   }
@@ -45,15 +54,19 @@ $(document).ready(function(){
   $("form#accountRegistration").submit(function(event){
     event.preventDefault();
     var name = $("input#name").val();
-    var amount = $("input#initialDeposit").val();
+    var amount = parseInt($("input#initialDeposit").val());
     var account = new BankAccount(name, amount);
     bank.addAccount(account);
     console.log(bank);
   });
   $("form#balanceChange").submit(function(event){
     event.preventDefault();
-    var depositAmount = $("input#deposit").val();
-    var withdrawal = $("input#withdrawal").val();
-    console.log(depositAmount, withdrawal);
+    var nameSearch = $("input#desiredAccount").val();
+    var depositAmount = parseInt($("input#deposit").val());
+    var withdrawalAmount = parseInt($("input#withdrawal").val());
+    var targetAccount = bank.findAccount(nameSearch);
+    console.log(targetAccount);
+    targetAccount.deposit(depositAmount);
+    console.log(bank.findAccount("mustafa"));
   });
 });
